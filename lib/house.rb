b6c36b6af1873051619216409e8d2a3901801fa4
Anna Smolentzov <annasmolentzov@gmail.com>
2018-10-17 09:43:18 -0600

    Add Room and House classes

---
+++ lib/house.rb
@@ -0,0 +1,39 @@
+class House
+  attr_reader :price, :address, :rooms
+  def initialize(price, address)
+    @price = price[1..-1].to_i
+    @address = address
+    @rooms = []
+  end
+  
+  def add_room(room)
+    @rooms << room
+  end
+  
+  def rooms_from_category(category)
+    @rooms.find_all do |room|
+      room.category == category
+    end
+  end
+  
+  def area
+    area = @rooms.map do |room|
+      room.area
+    end
+    area.sum
+  end
+  
+  def price_per_square_foot
+    (@price.to_f / area.to_f).round(2)
+  end
+  
+  def rooms_sorted_by_area
+    sorted_rooms = @rooms.sort_by do |room|
+      room.area
+    end
+    require 'pry'; bi
+    sorted_rooms
+  end
+  
+  
+end
---
+++ test/house_test.rb
@@ -0,0 +1,73 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/house'
+require './lib/room'
+
+class HouseTest < Minitest::Test
+  def setup
+    @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, 13)
+    @room_2 = Room.new(:bedroom, 11, 15)
+    @room_3 = Room.new(:living_room, 25, 15)
+    @room_4 = Room.new(:basement, 30, 41)
+  end
+  
+  def test_it_exists
+    assert_instance_of House, @house
+  end
+  
+  def test_it_can_report_price
+    assert_equal 400000, @house.price
+  end
+  
+  def test_it_can_report_address
+    assert_equal "123 sugar lane", @house.address
+  end
+  
+  def test_it_can_report_rooms
+    assert_equal [], @house.rooms
+  end
+  
+  def test_it_can_add_rooms
+    @house.add_room(@room_1)
+    assert_equal [@room_1], @house.rooms
+    @house.add_room(@room_2) 
+    assert_equal [@room_1, @room_2], @house.rooms
+  end
+  
+  def test_it_can_find_rooms_of_a_category
+    @house.add_room(@room_1)
+    @house.add_room(@room_2) 
+    @house.add_room(@room_3) 
+    @house.add_room(@room_4)
+    
+    assert_equal [@room_1, @room_2], @house.rooms_from_category(:bedroom) 
+    assert_equal [@room_4], @house.rooms_from_category(:basement)
+  end
+  
+  def test_it_can_calculate_house_area
+    @house.add_room(@room_1)
+    @house.add_room(@room_2) 
+    @house.add_room(@room_3) 
+    @house.add_room(@room_4)
+    assert_equal 1900, @house.area
+  end
+  
+  def test_it_can_calculate_price_per_square_foot
+    @house.add_room(@room_1)
+    @house.add_room(@room_2) 
+    @house.add_room(@room_3) 
+    @house.add_room(@room_4)
+    assert_equal 210.53, @house.price_per_square_foot
+  end
+  
+  def test_it_can_sort_rooms_by_area
+    @house.add_room(@room_1)
+    @house.add_room(@room_2) 
+    @house.add_room(@room_3) 
+    @house.add_room(@room_4)
+    assert_equal [@room_4, @room_3, @room_2, @room_1], @house.rooms_sorted_by_area
+  end
+  
+  
+end
